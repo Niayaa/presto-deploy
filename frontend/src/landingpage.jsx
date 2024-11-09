ae445b1848367e44d411b854a982ef6ee6f4325f
Niayaa <yongshi0627@gmail.com>
2024-11-09 15:30:40 +1100

    fixed

--- frontend/src/landingpage.jsx
+++ frontend/src/landingpage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from '../component/navbar';
 
 function LandingPage() {
   return (
     <div>
-      <Navbar />
       <main style={{ padding: '2rem', textAlign: 'center' }}>
         <h1>Welcome to Our App</h1>
         <p>Explore our templates, features, and join our community!</p>

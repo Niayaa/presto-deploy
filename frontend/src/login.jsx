06324e62fbfdad73d8495b7d50dfff09e40588a2
Liyongshi Chen(Nicole) <35710047+Niayaa@users.noreply.github.com>
2024-11-09 16:55:01 +1100

    Update login.jsx

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,7 +1,7 @@
 // src/pages/Login.jsx
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Login.css';
+import './login.css';
 import Navbar from '../component/navbar';
 
 function Login() {

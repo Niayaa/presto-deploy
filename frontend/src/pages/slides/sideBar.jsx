67bffe35ce0ed41524bee645f6824b543f71acb1
朱乐儿 <zhuleer@zhuleers-MacBook-Air.local>
2024-11-11 11:43:21 +1100

    add button to sidebar for adding image elements

--- frontend/src/pages/slides/sideBar.jsx
+++ frontend/src/pages/slides/sideBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './sideBar.css';
 
-const Sidebar = ({ onAddText }) => {
+const Sidebar = ({ onAddText, onAddImage }) => {
   return (
     <div className="sidebar">
       <button onClick={onAddText}>Text</button>
+      <button onClick={onAddImage}>Image</button>
     </div>
   );
 };

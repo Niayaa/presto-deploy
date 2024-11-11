1d87c8475bf5046135090906d87b6b499a8fcd72
朱乐儿 <zhuleer@zhuleers-MacBook-Air.local>
2024-11-11 18:17:47 +1100

    add button for adding video

--- frontend/src/pages/slides/sideBar.jsx
+++ frontend/src/pages/slides/sideBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './sideBar.css';
 
-const Sidebar = ({ onAddText, onAddImage }) => {
+const Sidebar = ({ onAddText, onAddImage, onAddVideo }) => {
   return (
     <div className="sidebar">
       <button onClick={onAddText}>Text</button>
       <button onClick={onAddImage}>Image</button>
+      <button onClick={onAddVideo}>Video</button>
     </div>
   );
 };

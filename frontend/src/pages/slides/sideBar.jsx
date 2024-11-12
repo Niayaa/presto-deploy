fcdb1d4c174c882188bf81762b4ea3b95b9f1fcd
朱乐儿 <zhuleer@zhuleers-MacBook-Air.local>
2024-11-12 20:38:26 +1100

    added code button into sidebar

--- frontend/src/pages/slides/sideBar.jsx
+++ frontend/src/pages/slides/sideBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './sideBar.css';
 
-const Sidebar = ({ onAddText, onAddImage, onAddVideo }) => {
+const Sidebar = ({ onAddText, onAddImage, onAddVideo, onAddCode }) => {
   return (
     <div className="sidebar">
       <button onClick={onAddText}>Text</button>
       <button onClick={onAddImage}>Image</button>
       <button onClick={onAddVideo}>Video</button>
+      <button onClick={onAddCode}>Code</button>
     </div>
   );
 };

cf51122da54fb6e7e4df08d6fff1555ea20a6fda
Niayaa <yongshi0627@gmail.com>
2024-11-14 12:53:08 +1100

    merge updated from main branch and added register file from main branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,34 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-
+import LandingPage from './pages/landingpage';
+import Login from './pages/login';
+import Register from './pages/register';
+import Navbar from '../component/navbar';
+import Dashboard from './pages/presentation/dashboard';
+import EditorPage from './pages/presentation/editorpage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from '../component/authcontext';
+import { PresentationProvider } from './pages/presentation/presentationContext';
 function App() {
   const [count, setCount] = useState(0);
   
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer noopener">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button id="counter" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <AuthProvider>
+      <PresentationProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />}/>
+            <Route path="/dashboard" element={<Dashboard />}/>
+            <Route path="/editor/:presentationId" element={<EditorPage />} />
+          </Routes>
+        </Router>
+      </PresentationProvider>
+    </AuthProvider>
     </>
   )
 }

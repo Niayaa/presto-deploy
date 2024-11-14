b033076d6c3fa3a779b9d329b571f21f835987b6
Niayaa <yongshi0627@gmail.com>
2024-11-14 12:53:28 +1100

    merge register file from main branch

---
+++ frontend/src/pages/register.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './register.css';
+
+function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password != confirmPassword) {
+      setError("Passwords do not match!");
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:5005/admin/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, password, confirmPassword }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Registration failed. Please try again.');
+      }
+
+      const data = await response.json();
+      const { token } = data;
+
+      localStorage.setItem('token', token);
+
+      navigate('/dashboard');
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <div className="register-card">
+        <h1>Create an Account</h1>
+        <p>Join us and start your journey</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Comfirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+          {error && <p className="error-message">{error}</p>}
+          <button type="submit" className="register-button">Sign Up</button>
+        </form>
+        <div className="footer-links">
+          <p>Already have an account?</p>
+          <Link to="/login" className="login-link">Log in</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
